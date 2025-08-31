d97ceed8b1c3f03031e8ab372d0b0d697e73cd36
Derek Young <dthomasy@gmail.com>
2025-08-31 15:25:33 -0700

    Add project id

--- frontend/src/app/api/[...path]/route.ts
+++ frontend/src/app/api/[...path]/route.ts
@@ -57,6 +57,7 @@ const GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID =
 const authClient = ExternalAccountClient.fromJSON({
   type: "external_account",
   audience: `//iam.googleapis.com/projects/${GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${GCP_WORKLOAD_IDENTITY_POOL_ID}/providers/${GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID}`,
+  projectId: GCP_PROJECT_ID,
   subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
   token_url: "https://sts.googleapis.com/v1/token",
   service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
