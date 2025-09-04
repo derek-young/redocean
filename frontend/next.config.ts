32f4f110c20294159e46ecfe0e421181d57ecdfa
Derek Young <dthomasy@gmail.com>
2025-09-03 20:03:05 -0700

    Remove app router api and add back proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
+const getBackendUrl = (): string => {
+  if (process.env.BACKEND_URL) {
+    return process.env.BACKEND_URL;
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:2550";
+  }
+
+  return "https://redocean-backend-989102844665.us-central1.run.app";
+};
+
 const nextConfig: NextConfig = {
-  // NOOP
+  output: "standalone",
+  async rewrites() {
+    const backendUrl = getBackendUrl();
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backendUrl}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
