b9579060eacdea57eff038ff3b36c63838d70097
Derek Young <dthomasy@gmail.com>
2025-10-05 14:52:19 -0700

    Use firebase service account to invoke GCR

--- frontend/src/gcpAuth.ts
+++ frontend/src/gcpAuth.ts
@@ -1,7 +1,18 @@
 import { GoogleAuth } from "google-auth-library";
 
 export async function getAuthHeaders(targetAudience: string) {
-  const auth = new GoogleAuth();
+  const options =
+    process.env.NODE_ENV === "production"
+      ? {
+          credentials: {
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            private_key: process.env.FIREBASE_PRIVATE_KEY,
+            project_id: process.env.FIREBASE_PROJECT_ID,
+          },
+        }
+      : undefined;
+  const auth = new GoogleAuth(options);
   const client = await auth.getIdTokenClient(targetAudience);
+
   return await client.getRequestHeaders();
 }
