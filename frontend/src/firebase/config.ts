7336af4a001ea051f688201aa5f082edf1164ac1
Derek Young <dthomasy@gmail.com>
2025-10-07 19:54:37 -0700

    Fix lint

--- frontend/src/firebase/config.ts
+++ frontend/src/firebase/config.ts
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV === "production") {
     measurementId: "G-DCVHHZMFV1",
   };
 } else {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   const firebaseConfig = require("./firebaseConfig");
   config = firebaseConfig.default;
 }
